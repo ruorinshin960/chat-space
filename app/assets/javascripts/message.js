1622228e8ab4e82c734deedb6a87048c73d2f2de
ruorinshin960 <ruorin.sin960za@gmail.com>
2019-09-22 18:40:34 +0900

    JSON done&error_method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,21 @@
 $(function() {
 
+  function buildHTML(message){
+
+    var html = `<div class="messages__members ">
+                  ${ message.user_name }
+                </div>
+                <div class="messages__timestamp">
+                  ${ message.time }
+                </div>
+                <div class="messages__chat ">
+                  <p class="message__content">
+                    ${ message.content }
+                  </p>
+                </div>`;
+        return html;
+    }
+
   $('#new_message').on('submit', function(e) {
      e.preventDefault();
      var formData = new FormData(this);
@@ -13,5 +29,16 @@ $(function() {
       processData: false,
       contentType: false
     })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__mask').val('');
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error')
+      $('.form__submit').prop('disabled', false);
+   })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,7 @@
-json.user_name  @message.user.name
-json.content  @message.content
-json.time  @message.created_at
-json.id  @message.id
+json.array! @messages do |message|
+  json.user_name  message.user.name
+  json.content  message.content
+  json.time  message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+  json.id  message.id
+  json.image message.image
+end
