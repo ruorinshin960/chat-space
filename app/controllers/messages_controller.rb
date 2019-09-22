f60920b9223b350c4adfc337540de9a82da6dbf7
ruorinshin960 <ruorin.sin960za@gmail.com>
2019-09-22 17:30:17 +0900

    jbuilder JSON-type

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,6 +17,7 @@ class MessagesController < ApplicationController
       flash.now[:alert] = 'メッセージを入力してください'
       render :index
     end
+   end
   end
 
   private
@@ -28,4 +29,5 @@ class MessagesController < ApplicationController
   def set_group
     @group = Group.find(params[:group_id])
   end
+
 end
