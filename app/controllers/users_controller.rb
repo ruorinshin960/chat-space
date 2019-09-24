5d2914e38824e60fdaa999823fa25cb7831a978a
ruorinshin960 <ruorin.sin960za@gmail.com>
2019-09-24 19:56:48 +0900

    chat_member addTextfield

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   
   def index
+    @users = User.where('name LIKE(?) and id NOT IN', "%#{params[:name]}%", "%#{current_user.id}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
